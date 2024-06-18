33c81d2e19f0e365618a424716d1e883aca1b5ef
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-06-19 01:09:44 +0530

    styling the parts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import { MdAutoDelete } from "react-icons/md";
+import { CiCircleCheck } from "react-icons/ci";
 
 function App() {
+
+  const [isCompleteScreen,setIsCompleateScreen] = useState(false);
   return (
     <div className="App">
       <h1>My ToDo</h1>
@@ -21,13 +26,18 @@ function App() {
         </div>
 
         <div className='btn-area'>
-          <button>Todo</button>
-          <button>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleateScreen(false)}>Todo</button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleateScreen(true)}>Completed</button>
         </div>
         <div className='todo-list'>
           <div className='todo-list-item'>
             <h1>task 1</h1>
             <p>description task 1</p>
+          </div>
+
+          <div>
+            <MdAutoDelete className='icon'/>
+          <CiCircleCheck className='check-icon'/>
           </div>
         </div>
       </div>
