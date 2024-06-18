bcb4c12c961e697d5652522c2c541dfbdf9f4be4
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-06-19 01:47:35 +0530

    create the adding item showing function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import React,{useState} from 'react';
 import './App.css';
-import { MdAutoDelete } from "react-icons/md";
-import { CiCircleCheck } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
 
 function App() {
 
-  const [isCompleteScreen,setIsCompleateScreen] = useState(false);
+  const [isCompleteScreen, setIsCompleteScreen] = useState (false);
+  const [allTodos, setTodos] = useState ([]);
+  const [newTitle, setNewTitle] = useState ('');
+  const [newDescription, setNewDescription] = useState ('');
+  const [completedTodos, setCompletedTodos] = useState ([]);
+  const [currentEdit,setCurrentEdit] = useState("");
+  const [currentEditedItem,setCurrentEditedItem] = useState("");
+
+  const handleAddTodo = ()=>{
+    let newTodoItem ={
+      title:newTitle,
+      description:newDescription
+    }
+    let updateTodoArr = [...allTodos];
+    updateTodoArr.push(newTodoItem);
+    setTodos(updateTodoArr);
+  }
+
   return (
     <div className="App">
       <h1>My ToDo</h1>
@@ -14,31 +31,36 @@ function App() {
         <div className='todo-input'>
           <div className='todo-input-item'>
             <label>Title</label>
-            <input type='text' placeholder='what is the task title?'/>
+            <input type='text'value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder='what is the task title?'/>
           </div>
           <div className='todo-input-item'>
             <label>Description</label>
-            <input type='text' placeholder='what is the task description?'/>
+            <input type='text' value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder='what is the task description?'/>
           </div>
           <div className='todo-input-item'>
-            <button type='button' className='primaryBtn'>Add</button>
+            <button type='button' onClick={handleAddTodo} className='primaryBtn'>Add</button>
           </div>
         </div>
 
         <div className='btn-area'>
-          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleateScreen(false)}>Todo</button>
-          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleateScreen(true)}>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>Todo</button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>Completed</button>
         </div>
         <div className='todo-list'>
-          <div className='todo-list-item'>
-            <h1>task 1</h1>
-            <p>description task 1</p>
+          {
+            allTodos.map((item, index)=>{
+              return(
+                <div className='todo-list-item' key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <div>
+            <MdDelete  className='icon'/>
+            <FaCheckCircle  className='check-icon'/>
           </div>
-
-          <div>
-            <MdAutoDelete className='icon'/>
-          <CiCircleCheck className='check-icon'/>
           </div>
+              )
+            })
+          }
         </div>
       </div>
     </div>
