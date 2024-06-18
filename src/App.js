af241dba3b1f782a7311e62cf938509745251251
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-06-19 00:23:54 +0530

    create main structure

--- src/App.js
+++ src/App.js
@@ -4,20 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My ToDo</h1>
+      <div className='todo-wrapper'>
+        <div className='todo-input'>
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type='text' placeholder='what is the task title?'/>
+          </div>
+          <div className='todo-input-item'>
+            <label>Description</label>
+            <input type='text' placeholder='what is the task description?'/>
+          </div>
+          <div className='todo-input-item'>
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+        </div>
+
+        <div className='btn-area'>
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+        <div className='todo-list'>
+          <div className='todo-list-item'>
+            <h1>task 1</h1>
+            <p>description task 1</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
